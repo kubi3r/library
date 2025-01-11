aedafbe36525739bfca6c7408b8c5b3f6c3c05c6
kubier <opisnerf@gmail.com>
2025-01-11 18:52:55 +0200

    Add function to create books

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+const library = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+function addBook(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read)
+    library.push(newBook)
 }
